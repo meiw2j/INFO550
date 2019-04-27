ea4d032d4ebba545c7950d2d6de95fbd0df395e4
meiw2j <meiw2j@gmail.com>
2019-04-27 19:38:06 -0400

    edit

--- app.R
+++ app.R
@@ -1,49 +1,45 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-library(tidyverse)
-library(ggplot2)
-library(shiny)
+# Rely on the 'WorldPhones' dataset in the datasets
+# package (which generally comes preloaded).
+library(datasets)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Percent of Patients treated for Drug/Alcohol Abuse vs. only Alcohol Abuse"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        selectInput("STATE","Choose a State", choices = unique(data$STATE))
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("plot")
-      )
-   )
+ui<- fluidPage(    
+  
+  # Give the page a title
+  titlePanel("Telephones by region"),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with one input
+    sidebarPanel(
+      selectInput("region", "Region:", 
+                  choices=colnames(WorldPhones)),
+      hr(),
+      helpText("Data from AT&T (1961) The World's Telephones.")
+    ),
+    
+    # Create a spot for the barplot
+    mainPanel(
+      plotOutput("phonePlot")  
+    )
+    
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+
+
+# Define a server for the Shiny app
+server<- function(input, output) {
   
-  library(repmis)
-  source_data("https://github.com/skhan8/n-ssats/blob/master/nssatspuf_2017.RData?raw=true")
-  data<-nssatspuf_2017[,c("STATE","B_PCT","A_PCT")] 
-  
-  output$plot<-renderPlot(
+  # Fill in the spot we created for a plot
+  output$phonePlot <- renderPlot({
     
-    ggplot(data = data %>% dplyr::filter(STATE==input$STATE),
-           aes(A_PCT, B_PCT))+
-      geom_point()
-  )
+    # Render a barplot
+    barplot(WorldPhones[,input$region]*1000, 
+            main=input$region,
+            ylab="Number of Telephones",
+            xlab="Year")
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
-
+shinyApp(ui, server)
